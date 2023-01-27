1ac52fc9d0bf0c7dd8b9f382b190f4c09c08c29d
Ovett <ovettmora17@gmail.com>
2023-01-27 14:36:54 -0600

    cambio en redireccionar a YT

--- src/assets/main.js
+++ src/assets/main.js
@@ -20,19 +20,21 @@ async function fetchData(urlApi) {
   try {
     const videos = await fetchData(API);
     let view = `
-    ${videos.items.map(video => `
-      <div class="group relative">
+    ${videos.items.map(video =>`
+    <ahref="https://youtube.com/watch?v=${video.id.videoId}"target="_blank">
+    <divclass="group relative">
         <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          <imgsrc="${video.snippet.thumbnails.high.url}"alt="${video.snippet.description}"class="w-full">
         </div>
-        <div class="mt-4 flex justify-between">
-          <h3 class="text-sm text-white">
-            <span aria-hidden="true" class="absolute inset-0"></span>
+        <divclass="mt-4 flex justify-between">
+          <h3class="text-sm text-gray-700">
+            <spanaria-hidden="true"class="absolute inset-0"></span>
             ${video.snippet.title}
           </h3>
         </div>
       </div>
+      </a>
     `).slice(0,4).join('')}
       
     `;

30842e2b37a452fd47be26a74df3d74f1b9ceec7
Ovett <ovettmora17@gmail.com>
2023-02-07 20:11:57 -0600

    Updates

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCvC4D8onUfXzvjTOM-dBfEA&part=snippet%2Cid&order=date&maxResults=4';
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCvC4D8onUfXzvjTOM-dBfEA&part=snippet%2Cid&order=date&maxResults=8';
 
 const content = null || document.querySelector('#content');
 
@@ -21,21 +21,20 @@ async function fetchData(urlApi) {
     const videos = await fetchData(API);
     let view = `
     ${videos.items.map(video =>`
-    <ahref="https://youtube.com/watch?v=${video.id.videoId}"target="_blank">
-    <divclass="group relative">
-        <div
-          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <imgsrc="${video.snippet.thumbnails.high.url}"alt="${video.snippet.description}"class="w-full">
+    <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank">
+    <div class="group relative">
+        <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}"alt="${video.snippet.description}"class="w-full">
         </div>
-        <divclass="mt-4 flex justify-between">
-          <h3class="text-sm text-gray-700">
-            <spanaria-hidden="true"class="absolute inset-0"></span>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-white">
+            <span aria-hidden="true"class="absolute inset-0"></span>
             ${video.snippet.title}
           </h3>
         </div>
       </div>
       </a>
-    `).slice(0,4).join('')}
+    `).slice(0,8).join('')}
       
     `;
     content.innerHTML = view;
